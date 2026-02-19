2bf017e449a73c39ff7a4b630306c1b9e831fa7c
Adriano Allef Pereira <adrianopereira951@outlook.com>
2026-02-19 11:44:33 -0300

    feat: cria sistema de caixa com cupom fiscal e cálculo de troco

--- Caixa-de-Mercado.py
+++ Caixa-de-Mercado.py
@@ -1,14 +1,22 @@
+carrinho = []
 total = 0
 while  True:
     try:
         #o codigo vai rodar para sempre
-        entrada = input("Digite o preço do produto:")
+        entrada_nome = str(input("\nDigite o nome do produto: "))
 
-        entrada = entrada.replace(",",".")
+        entrada_preco = input("Digite o preço do produto: ")
 
-        preco = float(entrada)
+        entrada_preco = entrada_preco.replace(",",".")
+
+        preco = float(entrada_preco)
+
+        add_carrinho = {"produto": entrada_nome, "preco": preco}
+
+        carrinho.append(add_carrinho)
 
         total += preco
+
     except:
         print("⚠️ Opa! Isso não é um número váido. Tente novamente.")
         continue
@@ -18,6 +26,36 @@ while  True:
     if continuar == "s" or continuar == "S":
             print(f"🛒Subtotal {total}")
     else:
-        print(f"💰Total da compra: {total}")
+        print("\n-----------------------")
+        print("\n---O Cupom Fiscal 🧾---\n")
+        
+        for i in carrinho:
+            print(f"Produto: {i['produto']} Preço: {i['preco']}")
+
+        print("\n-----------------------\n")
+        print(f"\n🛒 total da compra: {total}")
+
+        while True:
+            try:
+                valor_recebido = input("\n💰 Valor recebido: ")
+                print("\n-----------------------")
+                
+                valor_recebido = valor_recebido.replace(",",".")
+                
+                valor_recebido_tratado = float(valor_recebido)
+                break
+            except:
+                print("⚠️ Opa! Isso não é um número váido. Tente novamente.")
+                continue
+
+        troco = valor_recebido_tratado - total
+
+            
+
+        print(f"🛒 Total da compra: {total}")
+        print(f"💵 Valor pago: {valor_recebido_tratado}")
+        print(f"🪙 Troco: {troco}")
+            
+                
         print("❌Encerrando o sistema...")
         break  # <--- O freio de mão! Quebra o loop.

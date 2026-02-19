a082bdbb4a8c248b9a7b5a63963d666b3a8080eb
Adriano Allef Pereira <adrianopereira951@outlook.com>
2026-02-19 15:40:30 -0300

    - Separa a lógica principal em 3 funções: ,  e . - Adiciona o comando  para encerramento completo do aplicativo na opção 2. - Previne poluição de escopo resetando carrinho e total para novos clientes. - Limpa blocos de código comentados e inativos.

--- Caixa-de-Mercado.py
+++ Caixa-de-Mercado.py
@@ -1,61 +1,79 @@
-carrinho = []
-total = 0
-while  True:
-    try:
-        #o codigo vai rodar para sempre
-        entrada_nome = str(input("\nDigite o nome do produto: "))
+def imprimir_cupom(carrinho, total):
+    print("\n-----------------------")
+    print("\n---O Cupom Fiscal 🧾---\n")   
+    for i in carrinho:
+        print(f"Produto: {i['produto']} Preço: {i['preco']}")
 
-        entrada_preco = input("Digite o preço do produto: ")
+    print("\n-----------------------\n")
+    print(f"\n🛒 total da compra: {total}")
 
-        entrada_preco = entrada_preco.replace(",",".")
+    while True:
+        try:
+            valor_recebido = input("\n💰 Valor recebido: ")
+            print("\n-----------------------")
+            
+            valor_recebido = valor_recebido.replace(",",".")
+            
+            valor_recebido_tratado = float(valor_recebido)
+            break
+        except:
+            print("⚠️ Opa! Isso não é um número válido. Tente novamente.")
+            continue
 
-        preco = float(entrada_preco)
+    troco = valor_recebido_tratado - total
 
-        add_carrinho = {"produto": entrada_nome, "preco": preco}
-
-        carrinho.append(add_carrinho)
-
-        total += preco
-
-    except:
-        print("⚠️ Opa! Isso não é um número váido. Tente novamente.")
-        continue
-
-    continuar = input("Deseja continuar?s/n")
-
-    if continuar == "s" or continuar == "S":
-            print(f"🛒Subtotal {total}")
-    else:
-        print("\n-----------------------")
-        print("\n---O Cupom Fiscal 🧾---\n")
+    print(f"🛒 Total da compra: {total}")
+    print(f"💵 Valor pago: {valor_recebido_tratado}")
+    print(f"🪙 Troco: {troco}")
         
-        for i in carrinho:
-            print(f"Produto: {i['produto']} Preço: {i['preco']}")
+    print("❌Encerrando o sistema...")
 
-        print("\n-----------------------\n")
-        print(f"\n🛒 total da compra: {total}")
+def encerrar_sistema(encerrar):
+        if encerrar == "1":
+            iniciar_venda()
+        elif encerrar == "2":
+            print("❌Caixa fechado!")
+            exit()
+        else:
+            print("Digite um numero válido!")
+        
 
-        while True:
-            try:
-                valor_recebido = input("\n💰 Valor recebido: ")
-                print("\n-----------------------")
-                
-                valor_recebido = valor_recebido.replace(",",".")
-                
-                valor_recebido_tratado = float(valor_recebido)
-                break
-            except:
-                print("⚠️ Opa! Isso não é um número váido. Tente novamente.")
-                continue
+        
+    
+def iniciar_venda():
+    carrinho = []
+    total = 0
 
-        troco = valor_recebido_tratado - total
+    while  True:
+        try:
+            #o codigo vai rodar para sempre
+            entrada_nome = input("\nDigite o nome do produto: ")
 
-            
+            entrada_preco = input("Digite o preço do produto: ")
 
-        print(f"🛒 Total da compra: {total}")
-        print(f"💵 Valor pago: {valor_recebido_tratado}")
-        print(f"🪙 Troco: {troco}")
-            
-                
-        print("❌Encerrando o sistema...")
-        break  # <--- O freio de mão! Quebra o loop.
+            entrada_preco = entrada_preco.replace(",",".")
+
+            preco = float(entrada_preco)
+
+            add_carrinho = {"produto": entrada_nome, "preco": preco}
+
+            carrinho.append(add_carrinho)
+
+            total += preco
+
+        except:
+            print("⚠️ Opa! Isso não é um número váido. Tente novamente.")
+            continue
+
+        continuar = input("Deseja continuar?s/n")
+
+        if continuar == "s" or continuar == "S":
+                print(f"🛒Subtotal {total}")
+        else:
+            imprimir_cupom(carrinho=carrinho, total=total)
+        
+            encerrar = input("1 para Próximo Cliente, 2 para Encerrar: ")
+
+            encerrar_sistema(encerrar=encerrar)
+
+iniciar_venda()
